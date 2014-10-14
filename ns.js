1490474a0c9c8b1ee25b295fccea205ffbb73e18
Alex Logashov <admin@shiftarray.com>
2014-10-13 22:35:47 -0700

    Initial release

---
+++ ns.js
@@ -0,0 +1,51 @@
+(function(win) {
+
+"use strict";
+
+/**
+ * Generates namespace from string.
+ * @param  {string} namespace
+ * @param  {Object} object   
+ * @return {Object}          
+ */
+function ns(namespace, object) {
+	var part = win,
+		parts = namespace.split(".");
+	for (var i = 0, l = parts.length; i < l; i++) {
+	    if (!!object && i == l - 1) {
+	      part[parts[i]] = object;
+	    } else {
+	      if (!part[parts[i]]) {
+	        part[parts[i]] = {};
+	      }
+	      part = part[parts[i]];
+	    }
+	}
+	
+	return part;
+};
+
+win.ns = {
+
+	/**
+	 * Creates namespace object from namespace string.
+	 * @param  {string} namespace Period separated namespace.
+	 * @return {Object}           Last object created from namespace.
+	 */
+	require: function(namespace) {
+		return ns(namespace);
+	},
+
+	/**
+	 * Creates namespace object from namespace string.
+	 * @param  {string} namespace Namespace string.
+	 * @param  {Object} object    Object to assign last namespace part to.
+	 * @return {Object}           Returns object specified as second parameter.
+	 */
+	define: function(namespace, object) {
+		ns(namespace, object);
+		return object;
+	}
+};
+
+})(window);
